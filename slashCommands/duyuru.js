32cc6bd4dbe5c7821672c7819b218cdf4eee1e65
mikailerkart <99979466+mikailerkart@users.noreply.github.com>
2022-12-31 21:05:32 +0100

    duyuru.js
    duyuru

---
+++ slashCommands/duyuru.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+
+module.exports = {
+	category: "moderation",
+	data: new SlashCommandBuilder()
+		.setName('duyuru')
+		.setDescription('Bir duyuru verir başlıksız.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('Bir mesaj verin')
+                .setRequired(true))
+        .addChannelOption(option =>
+            option.setName("channel")
+            .setDescription("Bir kanal belirt")
+            .addChannelTypes(ChannelType.GuildText)),
+	async execute(client, interaction) {
+        
+        const message = await interaction.options.getString("message");
+        let channel = await interaction.options.getChannel("channel");
+
+        if(channel == null) channel = "988852575866617996"; // duyuru kanal
+
+
+        interaction.reply({ content: "duyuru yapıldı.", ephemeral: true});
+        await interaction.guild.channels.cache.find(c => c.id == channel).send(`${message}`);
+
+	},
+};
